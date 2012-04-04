96860159e6df70d3c9588125a775ccc1f5797f61
federico calvigioni <fcalvigioni@gmail.com>
2012-04-04 18:57:07 +0200

    l'acqua scarseggia

--- Esercizio4.js
+++ Esercizio4.js
@@ -1,75 +1,75 @@
 var cornerLeft = SIMPLEX_GRID([
-	[1],
+ [1],
   [2],
   [1]]);
 
 var poolBase = SIMPLEX_GRID([
-	[20],
+ [20],
   [1,-9,7],
   [1]]);
 
 var atriumBase = SIMPLEX_GRID([
-	[15],
+ [15],
   [17],
   [1]]);
 
 var scalesBase = SIMPLEX_GRID([
-	[3],
+ [3],
   [1,-3,13],
   [1]]);
 
 var rightBuilding = SIMPLEX_GRID([
-	[8],
+ [8],
   [12],
   [1]]);
 
 
 var rightCorner = T([0,1])([51,4])(SIMPLEX_GRID([
-	[1],
+ [1],
   [2],
   [1]]));
 
 var leftBuildingBase = T([1])([17])(SIMPLEX_GRID([
-	[8],
+ [8],
   [5],
   [1]]));
 
 var mainPool =T([1])([1])(SIMPLEX_GRID([
-	[20],
+ [20],
   [9],
   [0.6]]));
 
 var smallPool = T([1])([1]) (SIMPLEX_GRID([
-	[4],
+ [4],
   [11],
   [0.6]]));
 
 var boardSmallPool = SIMPLEX_GRID([
-	[4],
+ [4],
   [1],
   [1]]);
 
 var scales = STRUCT ([T([1])([1]),
-											 SIMPLEX_GRID([[0.375],[3],[1]]),T([0])([0.375]),
-											 SIMPLEX_GRID([[0.375],[3],[0.875]]),T([0])([0.375]),	
-											 SIMPLEX_GRID([[0.375],[3],[0.75]]),T([0])([0.375]),	
-											 SIMPLEX_GRID([[0.375],[3],[0.625]]),T([0])([0.375]),	
-											 SIMPLEX_GRID([[0.375],[3],[0.5]]),T([0])([0.375]),	
-											 SIMPLEX_GRID([[0.375],[3],[0.375]]),T([0])([0.375]),	
-											 SIMPLEX_GRID([[0.375],[3],[0.25]]),T([0])([0.375]),	
-											 SIMPLEX_GRID([[0.375],[3],[0.125]]),T([0])([0.375])]);
+            SIMPLEX_GRID([[0.375],[3],[1]]),T([0])([0.375]),
+            SIMPLEX_GRID([[0.375],[3],[0.875]]),T([0])([0.375]), 
+            SIMPLEX_GRID([[0.375],[3],[0.75]]),T([0])([0.375]), 
+            SIMPLEX_GRID([[0.375],[3],[0.625]]),T([0])([0.375]), 
+            SIMPLEX_GRID([[0.375],[3],[0.5]]),T([0])([0.375]), 
+            SIMPLEX_GRID([[0.375],[3],[0.375]]),T([0])([0.375]), 
+            SIMPLEX_GRID([[0.375],[3],[0.25]]),T([0])([0.375]), 
+            SIMPLEX_GRID([[0.375],[3],[0.125]]),T([0])([0.375])]);
 
 var baseStucture = STRUCT([cornerLeft,rightCorner,T([0])([1]),
-													poolBase,mainPool,
-													leftBuildingBase,T([0])([20]),
-													atriumBase,T([0])([15]),
-													scalesBase,scales,T([0])([3]),T([1])([4]),
-													rightBuilding,T([0])([8]),
-													smallPool,boardSmallPool,T([0])([4])
-													]);
+             poolBase,mainPool,
+             leftBuildingBase,T([0])([20]),
+             atriumBase,T([0])([15]),
+             scalesBase,scales,T([0])([3]),T([1])([4]),
+             rightBuilding,T([0])([8]),
+             smallPool,boardSmallPool,T([0])([4])
+             ]);
 
 var bigWall = T([0])([7.5])(T([1])([15])(SIMPLEX_GRID([
-	[20],
+ [20],
   [0.2],
   [3+1]])));
 
@@ -116,21 +116,23 @@ var portantLeftBuildingWall2 = T([0])([1])(T([1])([22])(SIMPLEX_GRID([ [8.2],[0.
 var portantLeftBuildingWall3 = T([0])([9])(T([1])([17])(SIMPLEX_GRID([ [0.2],[5],[3+1] ])));
 
 var leftBuildingStructure = STRUCT([leftroof,
-																		wallWindow,
-																		wall2,
-																		wall3,
-																		wall4,
-																		wall5,
-																		portantLeftBuildingWall1,
-																		portantLeftBuildingWall2,
-																		portantLeftBuildingWall3]);
+                  wallWindow,
+                  wall2,
+                  wall3,
+                  wall4,
+                  wall5,
+                  portantLeftBuildingWall1,
+                  portantLeftBuildingWall2,
+                  portantLeftBuildingWall3]);
 
 //costruzione destra
 var portantRightBuildingWall1 = SIMPLEX_GRID([ [7.9], [0.2], [3] ]);
 portantRightBuildingWall1 = T([0,1,2])([25.1,7.25, 1])(portantRightBuildingWall1);
+COLOR([0.76,0.29,0.13])(portantRightBuildingWall1);
 
 var portantRightBuildingWall2 = SIMPLEX_GRID([ [5.39], [0.2], [3] ]);
 portantRightBuildingWall2 = T([0,1,2])([37.1,11.5, 1])(portantRightBuildingWall2);
+COLOR([0.76,0.29,0.13])(portantRightBuildingWall1);
 
 var portantRightBuildingWall3 = SIMPLEX_GRID([ [13.343], [0.2], [4] ]);
 portantRightBuildingWall3 = T([0,1])([37.66,15.8])(portantRightBuildingWall3);
@@ -148,10 +150,10 @@ var verticalGrid = SIMPLEX_GRID([[0.1], REPLICA(3)([0.33,-3,0.33]), [3] ]);
 var orizontalGrid = SIMPLEX_GRID([  [0.1],REPLICA(2)([5.5]),[0.2,-2.6,0.2] ]);
 
 var windowsSmallPool = STRUCT([T([0])([47]),
-															 T([1])([5]),
-															 T([2])([1]),
-															 verticalGrid,
-															 orizontalGrid]);
+                T([1])([5]),
+                T([2])([1]),
+                verticalGrid,
+                orizontalGrid]);
 
 var externalWallRoom1 = SIMPLEX_GRID([ [0.1,-3.5],[3,-1,2.5],[3]]);
 externalWallRoom1 = T([0,1,2])([38.8,5,1 ])(externalWallRoom1);
@@ -222,7 +224,9 @@ var superiorGlassDoorOrizz = SIMPLEX_GRID([
 var superiorGlassDoor = STRUCT([ T([0,1,2])([40,13.67,1]), superiorGlassDoorVert, superiorGlassDoorOrizz ]);
 
 //la porta che unisce le due vetrate
-var bigDoor = T([0,1])([31,7.25])(SIMPLEX_GRID([[1], [6.5], [3]]));
+var bigDoor = T([0,1,2])([31,7.25,1])(SIMPLEX_GRID([[1], [6.5], [3]]));
+
+COLOR([0.15,0.372,0.17])(bigDoor);
 
 var rightRoof = T([0,1])([24,4])(SIMPLEX_GRID([[23],[13],[-1,-3,0.3]]));
 
@@ -231,20 +235,20 @@ var rightRoof = T([0,1])([24,4])(SIMPLEX_GRID([[23],[13],[-1,-3,0.3]]));
 
 //Struttura lato destro dell'edificio
 var rightBuildingStructure = STRUCT([ portantRightBuildingWall1,
-																			portantRightBuildingWall2,
-																			portantRightBuildingWall3,
-																			portantRightBuildingWall4,
-																			portantRightBuildingWall5,
-																			externalWallRoom1,
-																			externalWallRoom2,
-																			internalRoomWall,
-																			inferiorGlassWindow,
-																			superiorGlassWindow,
-																			inferiorGlassDoor,
-																			superiorGlassDoor,
-																			bigDoor,
-																			rightRoof,
-																			windowsSmallPool]);
+                   portantRightBuildingWall2,
+                   portantRightBuildingWall3,
+                   portantRightBuildingWall4,
+                   portantRightBuildingWall5,
+                   externalWallRoom1,
+                   externalWallRoom2,
+                   internalRoomWall,
+                   inferiorGlassWindow,
+                   superiorGlassWindow,
+                   inferiorGlassDoor,
+                   superiorGlassDoor,
+                   bigDoor,
+                   rightRoof,
+                   windowsSmallPool]);
 
 
 
@@ -255,26 +259,27 @@ var portantExternalWall2 = T([0,1])([1,1])(SIMPLEX_GRID([ [7], [0.2], [3] ]));
 
 //Colonnato
 var buildColumn = function (array_of_points){
-	var column = function (point){
+ var column = function (point){
     var x = point[0];
     var y = point[1];
     
-		var p1 = [x,y]
-		c1 = T([0,1,2])([x - 0.075, y - 0.1,1])(SIMPLEX_GRID([[0.15],[0.2],[3]]));
-		c2 = T([0,1,2])([x - 0.1, y - 0.075,1])(SIMPLEX_GRID([[0.2],[0.15],[3]]));
-		var column = STRUCT([c1,c2]);
-		DRAW(column);
-	}
+  var p1 = [x,y]
+  c1 = T([0,1,2])([x - 0.075, y - 0.1,1])(SIMPLEX_GRID([[0.15],[0.2],[3]]));
+  c2 = T([0,1,2])([x - 0.1, y - 0.075,1])(SIMPLEX_GRID([[0.2],[0.15],[3]]));
+  var column = STRUCT([c1,c2]);
+    COLOR([0.4,0.08,0.21])(column);
+  DRAW(column);
+ }
 
-	MAP(column)(array_of_points);
+ MAP(column)(array_of_points);
 }
 
 var murature = STRUCT([bigWall,
-											 banches,
-											 leftBuildingStructure,
-											 rightBuildingStructure,
-											 portantExternalWall,
-											 portantExternalWall2]);
+            banches,
+            leftBuildingStructure,
+            rightBuildingStructure,
+            portantExternalWall,
+            portantExternalWall2]);
 
 //costruzione colonne (stesse coordinate della planimetria)
 
@@ -288,18 +293,21 @@ var baseTavolo = T([0,2])([-0.25,0.5])(SIMPLEX_GRID([[3.5],[1.2],[0.2]]));
 var tavolo = STRUCT([piediTavolo,baseTavolo]);
 
 var piediTavolo1 = SIMPLEX_GRID([[0.7],[0.5,-2,0.5],[0.5]]);
-var baseTavolo1 = T([0,2])([-0.25,0.5])(SIMPLEX_GRID([[1.2],[3],[0.2]]));
+var baseTavolo1 = T([0,2])([-0.25,0.5])(SIMPLEX_GRID([[1.2],[3.5],[0.2]]));
 
 var tavolo1 = STRUCT([piediTavolo1,baseTavolo1]);
 
 
+COLOR([0.96,0.8,0.69])(baseStucture);
+COLOR([1,0,0])(tavolo);
+COLOR([1,0,0])(tavolo1);
+COLOR([1,1,1])(rightRoof);
+COLOR([1,1,1])(leftroof);
 
 var pavilion = STRUCT([baseStucture,murature, T([0,1,2])([2,12,1]),tavolo,T([0,1])([21,-10]),tavolo1]); 
 
+
+
+
+
 DRAW(pavilion);
-
-
-
-
-
-
